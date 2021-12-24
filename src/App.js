37b3fdc28b267648264c726f1fd4b3067bf0c478
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-12-24 06:01:20 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import {v4 as uuidv4 } from 'uuid';
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import {useState } from 'react';
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import FeedbackData from './data/FeedbackData';
+
+function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback,...feedback])
+    }
+
+const deleteFeedback = (id) => {
+    if(window.confirm('Are You Sure You Want To Delete?')){
+        setFeedback(feedback.filter((item) => item.id !== id))
+    }
+}
+   
+    return (
+        <Router>
+        <Header/>
+        <div className="container">
+            <Routes>
+            <Route exact path='/' element={
+                <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                </>
+            }> 
+            
+            </Route>
+            <Route path='/about' element={<AboutPage/>}/>
+            </Routes>
+        </div>
+        </Router>
+    )
+}
+
+export default App
