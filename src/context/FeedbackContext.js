5de326ed4e5645b387fd23854f5936d61ba88451
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-12-27 23:09:59 +0000

    update: changed to grab the data from the JSON server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,51 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect} from "react";
 import {v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext  = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            rating: 10,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 2,
-            rating: 9,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 3,
-            rating: 8,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 4,
-            rating: 8,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 5,
-            rating: 5,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 6,
-            rating: 11,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 7,
-            rating: 12,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          }
-    ])
-
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
       item: {},
       edit: false
     })
+
+    useEffect(() => {
+    fetchFeedback()
+    }, [])
+    // get the feedback
+    const fetchFeedback = async () => {
+      const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+      const data = await response.json()
+      setFeedback(data)
+      setIsLoading(false)
+    }
+
+
     // this will add in the feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
@@ -75,6 +52,7 @@ export const FeedbackProvider = ({children}) => {
     return <FeedbackContext.Provider value={{
             feedback,
             feedbackEdit,
+            isLoading,
             deleteFeedback,
             addFeedback,
             editFeedback,
